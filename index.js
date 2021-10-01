18fd0e3ae7d8105dd7458469c9a13803a3b66cca
Kevin Balicot <kevinbalicot@gmail.com>
2021-10-01 11:24:15 +0200

    Initial commit | :tada:

---
+++ index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const pathToSwaggerUi = require('swagger-ui-dist').absolutePath();
+const swaggerJs = fs.readFileSync(`${pathToSwaggerUi}/swagger-ui-bundle.js`, 'utf-8');
+const swaggerCss = fs.readFileSync(`${pathToSwaggerUi}/swagger-ui.css`, 'utf-8');
+
+module.exports = function(req, res, next) {
+    if (req.url === '/doc.json') {
+        res.setHeader('Content-Type', 'application/json');
+
+        return res.end(JSON.stringify(this.api));
+    }
+
+    if (req.url === '/doc.html') {
+        res.setHeader('Content-Type', 'text/html');
+
+        return res.end(`
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <meta charset="utf-8">
+
+                    <title>Swagger API Doc</title>
+                    <script>${swaggerJs}</script>
+                    <style>${swaggerCss}</style>
+                </head>
+                <body>
+                    <section id="swagger-container"></section>
+                    <script>
+                        SwaggerUIBundle({
+                            url: '/api/doc.json',
+                            dom_id: '#swagger-container',
+                        });
+                    </script>
+                </body>
+            </html>
+        `);
+    }
+
+    next(req, res, next);
+};
